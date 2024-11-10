106dd44cf614a0d56a6a176e8e06f891b61a401e
pintukumarsingh29 <pintusingh68902@gmail.com>
2024-11-10 20:07:35 +0530

    Learning Python for data science

---
+++ controlflow.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster ride")
+height = int(input("What is your height in centimeters?"))
+if height > 120:
+    print("You can ride")
+else:
+    print("You can't ride")
---
+++ intoPython.py
@@ -0,0 +1,19 @@
+# Line of code using input() function to get input from the user
+"""print("Hello " + input("What is your name?") + "!")"""
+
+# Using variable to store input values
+#name = input("What is your name?")
+#print(len(name))
+
+"""We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. 
+Write 3 lines of code to switch the contents of the variables. You are not allowed to type the words "milk" or "juice". 
+You are only allowed to use variables to solve this exercise."""
+
+"""glass1 = "milk"
+glass2 = "Juice"
+
+demo = glass2
+glass2 = glass1
+glass1 = demo
+
+print(glass1 + " " + glass2)"""
