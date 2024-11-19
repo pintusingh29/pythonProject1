37143534b8de1312e2173587d2412735807e3915
pintukumarsingh29 <pintusingh68902@gmail.com>
2024-11-19 09:53:15 +0530

    Learning Python for data science

---
+++ codingChallenge.py
@@ -0,0 +1,28 @@
+"""
+Leap Year
+💪 This is a difficult challenge! 💪
+
+Write a program that returns True or False whether if a given year is a leap year.
+A normal year has 365 days, leap years have 366, with an extra day in February.
+
+This is how you work out whether if a particular year is a leap year.
+- on every year that is divisible by 4 with no remainder
+- except every year that is evenly divisible by 100 with no remainder
+- unless the year is also divisible by 400 with no remainder
+"""
+
+# Year = int(input("Enter a year: "))
+#
+# def is_leap_year(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 print("Leap Year")
+#             else:
+#                 print("Not Leap Year")
+#         else:
+#             print("Leap Year")
+#     else:
+#         print("Not Leap Year")
+#
+# is_leap_year(Year)
--- controlflow.py
+++ controlflow.py
@@ -1,6 +1,29 @@
-print("Welcome to the rollercoaster ride")
-height = int(input("What is your height in centimeters?"))
-if height > 120:
-    print("You can ride")
+# print("Welcome to the rollercoaster ride")
+# height = int(input("What is your height in centimeters? "))
+# if height > 120:
+#     age = int(input("How old are you? "))
+#     if age >= 18:
+#         print("Ticket $12")
+#     elif age <= 18:
+#         print("Ticket $7")
+# else:
+#     print("You can't ride")
+
+"""
+NumCheck = int(input("Enter the number to check if ODD or EVEN"))
+if NumCheck % 2 == 0:
+    print(NumCheck," is an even number")
 else:
-    print("You can't ride")
+    print(NumCheck," is an odd number")
+"""
+# weight = 67
+# height = 1.85
+#
+# bmi = weight / (height ** 2)
+#
+# if bmi < 18.5:
+#     print("underweight")
+# elif bmi == 18.5 and bmi < 25:
+#     print("normal weight")
+# else:
+#     print("overweight")
